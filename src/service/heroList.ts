7e6e28182036876f80ba1794547d98b88631d2ea
SorawitFM <fmzazadds2@gmail.com>
2024-01-08 22:38:58 +0700

    commit 7 ( GET API  in folder service , Set part alias )

--- src/service/heroList.ts
+++ src/service/heroList.ts
@@ -1,7 +1,18 @@
+import axios from 'axios'
 import React from 'react'
+import { handleResponse } from '../utils/handleResponse'
 
-const heroList = () => {
-    return
+
+
+export const heroListServie = {
+    getHeroList: async (): Promise<IResponse> => {
+        try {
+            const response = await axios.get(`https://api.opendota.com/api/heroStats`)
+            //console.log(response)
+            return handleResponse.success(response)
+        } catch (error: any) {
+            return handleResponse.error(error)
+        }
+
+    }
 }
-
-export default heroList

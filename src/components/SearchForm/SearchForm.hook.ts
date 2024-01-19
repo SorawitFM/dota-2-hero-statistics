2c38b8b6781fcbcd348b61c6321694c9948c40f1
SorawitFM <fmzazadds2@gmail.com>
2024-01-19 16:38:26 +0700

    commit 15 ( Edit functions  sortBy and add async for calculation functions )

--- src/components/SearchForm/SearchForm.hook.ts
+++ src/components/SearchForm/SearchForm.hook.ts
@@ -95,7 +95,7 @@ const useSearchForm = () => {
                         winRateAncient: (hero['6_win'] / hero['6_pick']) * 100,
                         winRateDivine: (hero['7_win'] / hero['7_pick']) * 100,
                         winRateImmortal: (hero['8_win'] / hero['8_pick']) * 100,
-                        winRateAverage: calWinRateAverage(hero),
+                        winRateAverage: await calWinRateAverage(hero),
                         pickHerald: hero['1_pick'],
                         pickGuardian: hero['2_pick'],
                         pickCrusader: hero['3_pick'],
@@ -110,8 +110,8 @@ const useSearchForm = () => {
                             + hero['5_pick'] + hero['6_pick']
                             + hero['7_pick'] + hero['8_pick'],
 
-                        winRate: winRate(hero, rank),
-                        pickValue: pickValue(hero, rank)
+                        winRate: await winRate(hero, rank),
+                        pickValue: await pickValue(hero, rank)
                     })
             }
             console.log('check2', heroList)
@@ -176,134 +176,24 @@ const useSearchForm = () => {
         console.log('check fillter3', roleFilter)
         console.log('check before source by', rank)
         console.log('check before source by 2', sort)
-        return sortBy(roleFilter, sort, rank)
+        return sortBy(roleFilter, sort)
     }
 
-    const sortBy = (data: IHeroListUpdate[], sort: number, rank: number) => {
+    const sortBy = (data: IHeroListUpdate[], sort: number) => {
         switch (sortList[sort]) {
             case 'Name':
                 return data.sort((a, b) => a.localized_name > b.localized_name ? 1 : b.localized_name > a.localized_name ? -1 : 0)
             case 'Win rate':
-                switch (rankList[rank].name) {
-                    case 'Herald':
-                        return data.sort((b, a) => a['1_win'] / a['1_pick'] - b['1_win'] / b['1_pick'])
-                    case 'Guardian':
-                        return data.sort((b, a) => a['2_win'] / a['2_pick'] - b['2_win'] / b['2_pick'])
-                    case 'Crusader':
-                        return data.sort((b, a) => a['3_win'] / a['3_pick'] - b['3_win'] / b['3_pick'])
-                    case 'Archon':
-                        return data.sort((b, a) => a['4_win'] / a['4_pick'] - b['4_win'] / b['4_pick'])
-                    case 'Legend':
-                        return data.sort((b, a) => a['5_win'] / a['5_pick'] - b['5_win'] / b['5_pick'])
-                    case 'Ancient':
-                        return data.sort((b, a) => a['6_win'] / a['6_pick'] - b['6_win'] / b['6_pick'])
-                    case 'Divine':
-                        return data.sort((b, a) => a['7_win'] / a['7_pick'] - b['7_win'] / b['7_pick'])
-                    case 'Immortal':
-                        return data.sort((b, a) => a['8_win'] / a['8_pick'] - b['8_win'] / b['8_pick'])
-                    case 'All Ranks':
-                        return data.sort((b, a) =>
-                            + a['1_win'] / a['1_pick']
-                            + a['2_win'] / a['2_pick']
-                            + a['3_win'] / a['3_pick']
-                            + a['4_win'] / a['4_pick']
-                            + a['5_win'] / a['5_pick']
-                            + a['6_win'] / a['6_pick']
-                            + a['7_win'] / a['7_pick']
-                            + a['8_win'] / a['8_pick']
-                            - b['1_win'] / b['1_pick']
-                            - b['2_win'] / b['2_pick']
-                            - b['3_win'] / b['3_pick']
-                            - b['4_win'] / b['4_pick']
-                            - b['5_win'] / b['5_pick']
-                            - b['6_win'] / b['6_pick']
-                            - b['7_win'] / b['7_pick']
-                            - b['8_win'] / b['8_pick']
-                        )
-                    default:
-                        return data.sort((b, a) =>
-                            + a['1_win'] / a['1_pick']
-                            + a['2_win'] / a['2_pick']
-                            + a['3_win'] / a['3_pick']
-                            + a['4_win'] / a['4_pick']
-                            + a['5_win'] / a['5_pick']
-                            + a['6_win'] / a['6_pick']
-                            + a['7_win'] / a['7_pick']
-                            + a['8_win'] / a['8_pick']
-                            - b['1_win'] / b['1_pick']
-                            - b['2_win'] / b['2_pick']
-                            - b['3_win'] / b['3_pick']
-                            - b['4_win'] / b['4_pick']
-                            - b['5_win'] / b['5_pick']
-                            - b['6_win'] / b['6_pick']
-                            - b['7_win'] / b['7_pick']
-                            - b['8_win'] / b['8_pick']
-                        )
-                }
+                return data.sort((b, a) => a.winRate - b.winRate)
             case 'Pick':
-                switch (rankList[rank].name) {
-                    case 'Herald':
-                        return data.sort((b, a) => a['1_pick'] - b['1_pick'])
-                    case 'Guardian':
-                        return data.sort((b, a) => a['2_pick'] - b['2_pick'])
-                    case 'Crusader':
-                        return data.sort((b, a) => a['3_pick'] - b['3_pick'])
-                    case 'Archon':
-                        return data.sort((b, a) => a['4_pick'] - b['4_pick'])
-                    case 'Legend':
-                        return data.sort((b, a) => a['5_pick'] - b['5_pick'])
-                    case 'Ancient':
-                        return data.sort((b, a) => a['6_pick'] - b['6_pick'])
-                    case 'Divine':
-                        return data.sort((b, a) => a['7_pick'] - b['7_pick'])
-                    case 'Immortal':
-                        return data.sort((b, a) => a['8_pick'] - b['8_pick'])
-                    case 'All Ranks':
-                        return data.sort((b, a) =>
-                            + a['1_pick']
-                            + a['2_pick']
-                            + a['3_pick']
-                            + a['4_pick']
-                            + a['5_pick']
-                            + a['6_pick']
-                            + a['7_pick']
-                            + a['8_pick']
-                            - b['1_pick']
-                            - b['2_pick']
-                            - b['3_pick']
-                            - b['4_pick']
-                            - b['5_pick']
-                            - b['6_pick']
-                            - b['7_pick']
-                            - b['8_pick']
-                        )
-                    default:
-                        return data.sort((b, a) =>
-                            + a['1_pick']
-                            + a['2_pick']
-                            + a['3_pick']
-                            + a['4_pick']
-                            + a['5_pick']
-                            + a['6_pick']
-                            + a['7_pick']
-                            + a['8_pick']
-                            - b['1_pick']
-                            - b['2_pick']
-                            - b['3_pick']
-                            - b['4_pick']
-                            - b['5_pick']
-                            - b['6_pick']
-                            - b['7_pick']
-                            - b['8_pick']
-                        )
-                }
+                return data.sort((b, a) => a.pickValue - b.pickValue)
             default:
                 return data.sort((a, b) => a.localized_name > b.localized_name ? 1 : b.localized_name > a.localized_name ? -1 : 0)
         }
     }
 
     //Calculate average win rate 
-    const calWinRateAverage = (hero: any) => {
+    const calWinRateAverage = async (hero: any) => {
         return ((
             + hero['1_win'] + hero['2_win']
             + hero['3_win'] + hero['4_win']
@@ -319,7 +209,7 @@ const useSearchForm = () => {
         )
     }
 
-    const winRate = (hero: any, rank: number) => {
+    const winRate = async (hero: any, rank: number) => {
         const meme: string = rankList[rank].name
         switch (meme) {
             case 'Herald':
@@ -339,12 +229,12 @@ const useSearchForm = () => {
             case 'Immortal':
                 return (hero['8_win'] / hero['8_pick']) * 100
             case 'All Ranks':
-                return calWinRateAverage(hero)
+                return await calWinRateAverage(hero)
             default:
-                return calWinRateAverage(hero)
+                return await calWinRateAverage(hero)
         }
     }
-    const pickValue = (hero: any, rank: number) => {
+    const pickValue = async (hero: any, rank: number) => {
         const meme: string = rankList[rank].name
         switch (meme) {
             case 'Herald':
@@ -364,13 +254,13 @@ const useSearchForm = () => {
             case 'Immortal':
                 return hero['8_pick']
             case 'All Ranks':
-                return totalPick(hero)
+                return await totalPick(hero)
             default:
-                return totalPick(hero)
+                return await totalPick(hero)
         }
     }
 
-    const totalPick = (hero: any) => {
+    const totalPick = async (hero: any) => {
         return (
             + hero['1_pick'] + hero['2_pick']
             + hero['3_pick'] + hero['4_pick']
@@ -388,13 +278,17 @@ const useSearchForm = () => {
     }, [])
 
     useEffect(() => {
+        callData()
         const data = filterHero(fetchHero.data, keyword, rank, attribute, role, sort)
         setHeroList({
             data: data,
             loading: false,
             error: null
         })
+
     }, [keyword, rank, attribute, role, sort])
+
+
 
 
 
